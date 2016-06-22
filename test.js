0c9f6b827732ffcb7e17e5265eccb8851967d624
Kyle E. Mitchell <kyle@kemitchell.com>
2016-06-21 22:24:29 -0700

    Test sequence numbers yielded by .append() calls

--- test.js
+++ test.js
@@ -29,3 +29,24 @@ tape('can add', function (t) {
     })
   })
 })
+
+tape('append sequence numbers', function (t) {
+  var lgs = logs(memdb(), {valueEncoding: 'json'})
+  var returned = 0
+
+  lgs.append('mathias', {hello: 'world'}, function (err, seq) {
+    t.ifError(err, 'no error')
+    t.equal(seq, 1, 'first gets seq 1')
+    returned++
+    done()
+  })
+  lgs.append('mathias', {hej: 'verden'}, function (err, seq) {
+    t.ifError(err, 'no error')
+    t.equal(seq, 2, 'second gets seq 2')
+    returned++
+    done()
+  })
+  function done () {
+    if (returned === 2) t.end()
+  }
+})
